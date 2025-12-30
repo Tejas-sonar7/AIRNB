2e0a2a62aeb9f1d85338324469bf29b5d16e24e3
Tejas-sonar7 <tejasmore101204@gmail.com>
2025-12-30 19:34:50 +0530

    Fix port and root route for Render

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 const initData = require("./data.js")
 const Listing = require ("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLAS_URI;
 
 main().then(() =>{
     console.log("database connected succesfully")
@@ -18,7 +18,7 @@ async function main(){
 const initDB = async () =>{
     await Listing.deleteMany({});
     initData.data = initData.data.map(listing => { 
-        listing.owner = "69258633717638eb9cdccc49"; // Replace with a valid User ObjectId from your database
+        listing.owner = "6953ca96c5d1afe305386bb4"; // Replace with a valid User ObjectId from your database
         return listing;
     });
     await Listing.insertMany(initData.data);
